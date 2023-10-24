c6feb21d3cd6567c1b7c67e3ffb1007eee3c5473
ybalbino <yasmin.balbino@zup.com.br>
2023-10-24 11:26:10 -0300

    iniciando projeto

---
+++ src/main/java/com/example/projetoXmem/ProjetoXmemApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetoXmem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoXmemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoXmemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetoXmem/ProjetoXmemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoXmem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoXmemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
